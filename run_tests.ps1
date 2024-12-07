9b09cff3dd7567f05201a18862510a67f6810d56
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-07 20:08:40 +0100

    Add tests

--- run_tests.ps1
+++ run_tests.ps1
@@ -9,19 +9,25 @@ New-Item -ItemType Directory "$OUT_DIR"
 
 
 g++ -Wall -Wextra -Werror `
+    -L build/googletest-1.15.2/build/lib `
     -L build/glfw-3.4/build/src `
     -L build/freeglut-3.6.0/build/lib `
+    -I build/googletest-1.15.2/googletest/include `
+    -I build/googletest-1.15.2/googlemock/include `
     -I build/glfw-3.4/include `
     -I build/freeglut-3.6.0/include `
     -I build/glad/include `
     -I build/glad/src `
-    src/main.cpp `
-    -o "$OUT_DIR/main.exe" `
+    tests/test_main.cpp `
+    -o "$OUT_DIR/test_main.exe" `
     -l glfw3 `
     -l freeglut `
+    -l gtest `
+    -l gmock `
+    -l gtest_main `
     -lgdi32
      
 
 if ($?) {
-    . "$OUT_DIR/main.exe"
+    . "$OUT_DIR/test_main.exe"
 }
---
+++ tests/test_main.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+
+TEST(Experiments, FileReading) {
+    ASSERT_EQ(1, 1);    
+}
