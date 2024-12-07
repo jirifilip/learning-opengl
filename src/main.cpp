511cd27ff8fb815b16ebd22445185fb5eedc529d
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-07 14:22:47 +0100

    Rewrite example by hand

--- src/main.cpp
+++ src/main.cpp
@@ -5,39 +5,54 @@
 #include <GLFW/glfw3.h>
 
 
-int main() {
-    std::cout << "Hello world" << std::endl;
+void abortProgram(const std::string message) {
+    glfwTerminate();
+    std::cerr << message << std::endl;
+    std::abort();
+}
 
+
+void handleKeyPress(
+        [[maybe_unused]] GLFWwindow* window,
+        int key,
+        [[maybe_unused]] int scancode,
+        [[maybe_unused]] int action,
+        [[maybe_unused]] int mods
+    ) {
+    
+    std::cout << "Key pressed: " << key << std::endl;
+}
+
+
+int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
+
+    if (window == nullptr) {
+        abortProgram("Failed to initialize window");
     }
+
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }    
+    // TODO: explore further
+    auto loaded = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    if (!loaded) {
+        abortProgram("Failed to initialize GLAD");
+    }
 
     glViewport(0, 0, 800, 600);
 
-    while(!glfwWindowShouldClose(window))
-    {
+    glfwSetKeyCallback(window, handleKeyPress);
+
+    while (!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
-        glfwPollEvents();    
+        glfwPollEvents();
     }
 
-        
     glfwTerminate();
     return 0;
 }
