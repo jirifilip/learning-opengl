ab51db799e6c32cd103d8e375168f5b622d639d7
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-08 15:24:10 +0100

    Draw a rectangle using element buffer

--- src/main.cpp
+++ src/main.cpp
@@ -177,12 +177,14 @@ int main() {
 
 
     float vertices[] { 
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
-        -0.9f, -0.8f, 0.0f,
-        -0.95f, -0.85f, 0.0f,
-        -1.0f, -1.0f, 0.0f,
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f  // top left
+    };
+    unsigned int indices[] {
+        0, 1, 3,  // first triangle
+        1, 2, 3, // second triangle
     };
     
     Shader vertexShader{ "src/shader.vert", GL_VERTEX_SHADER };
@@ -192,9 +194,10 @@ int main() {
     ShaderProgram program { shaders };
 
 
-    unsigned int vertexBufferID, vertexArrayID;
+    unsigned int vertexBufferID, vertexArrayID, elementBufferID;
     glGenVertexArrays(1, &vertexArrayID);
     glGenBuffers(1, &vertexBufferID);
+    glGenBuffers(1, &elementBufferID);
 
     glBindVertexArray(vertexArrayID);
 
@@ -205,6 +208,9 @@ int main() {
         vertices,
         GL_STATIC_DRAW
     );
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
@@ -217,7 +223,8 @@ int main() {
 
         program.use();
         glBindVertexArray(vertexArrayID);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
      
         glfwPollEvents();
         processInput(window.get());
