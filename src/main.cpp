1b0c206732ad0a60b7965eda1c3332dffb4ca70e
Jiří Filip <j.f.ilip@seznam.cz>
2024-12-11 10:17:17 +0100

    Rotating cubes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <functional>
 #include <cmath>
+#include <random>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -88,6 +89,18 @@ int main() {
         -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
 
     Shader vertexShader{ "shaders/shader.vert", GL_VERTEX_SHADER };
     Shader fragmentShader { "shaders/shader.frag", GL_FRAGMENT_SHADER };
@@ -120,42 +133,50 @@ int main() {
     );
     glEnableVertexAttribArray(1);
 
+    glm::mat4 identity { 1 };
+    auto viewMatrix = glm::translate(identity, glm::vec3{ 0, 0, -3 });
+    [[maybe_unused]] auto perspectiveProjectionMatrix = glm::perspective(
+        glm::radians(45.0f), static_cast<float>(800 / 600), 0.1f, 100.0f
+    );
+    // TODO: come back to this
+    [[maybe_unused]] auto orthographicProjectionMatrix = glm::ortho(
+        0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f
+    );
+
     shaderProgram.use();
     shaderProgram.setUniform("textureSampler1", 0);
     shaderProgram.setUniform("textureSampler2", 1);
+    shaderProgram.setUniform("viewMatrix", viewMatrix);
+    shaderProgram.setUniform("projectionMatrix", perspectiveProjectionMatrix);
 
     while (!glfwWindowShouldClose(window.get())) {
         glfwSwapBuffers(window.get());
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glm::mat4 identity { 1 };
-        auto modelMatrix = glm::rotate(
-            identity,
-            static_cast<float>(glfwGetTime()) * glm::radians(50.0f),
-            glm::vec3{ 0.5, 1, 0 }
-        );
-        auto viewMatrix = glm::translate(identity, glm::vec3{ 0, 0, -3 });
-        [[maybe_unused]] auto perspectiveProjectionMatrix = glm::perspective(
-            glm::radians(45.0f), static_cast<float>(800 / 600), 0.1f, 100.0f
-        );
-        // TODO: come back to this
-        [[maybe_unused]] auto orthographicProjectionMatrix = glm::ortho(
-            0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f
-        );
-        shaderProgram.setUniform("modelMatrix", modelMatrix);
-        shaderProgram.setUniform("viewMatrix", viewMatrix);
-        shaderProgram.setUniform("projectionMatrix", perspectiveProjectionMatrix);
         
         wallTexture.use(GL_TEXTURE0);
         faceTexture.use(GL_TEXTURE1);
 
         glBindVertexArray(vertexArrayID);
-        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        int i = 0;
+        for (auto& cubePosition : cubePositions) {
+            auto modelTranslate = glm::translate(identity, cubePosition);
+            auto modelRotate = glm::rotate(
+                modelTranslate,
+                static_cast<float>(glfwGetTime()) * glm::radians((i + 1) * 20.0f),
+                glm::vec3{ 0.5, 1, 0 }
+            );
+            
+            shaderProgram.setUniform("modelMatrix", modelRotate);
+            // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            i++;
+        }
         glBindVertexArray(0);
-     
+
         glfwPollEvents();
         processInput(window.get());
     }
